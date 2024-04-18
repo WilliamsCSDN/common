f5d51c1cba3debb816a8e9a94ef756fc16bf0d44
wuzhaoyan <wuzhaoyan@iboxpay.com>
2024-04-18 09:49:51 +0800

    feat: 初始化项目

---
+++ williams-project-web/src/test/java/org/williams/project/ProjectWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.williams.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
