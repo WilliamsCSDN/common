29b7bc94ffa049e9de6d59fceb1748ba72e56292
wuzhaoyan <wuzhaoyan@ips.com>
2025-05-09 16:39:35 +0800

    feat: 新增junit测试方式

---
+++ williams-project-web/src/test/java/org/williams/project/Junit3Test.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2011-present ShenZhen iBOXCHAIN Information Technology Co.,Ltd.
+ *
+ * All right reserved.
+ *
+ * This software is the confidential and proprietary
+ * information of iBOXCHAIN Company of China.
+ * ("Confidential Information"). You shall not disclose
+ * such Confidential Information and shall use it only
+ * in accordance with the terms of the contract agreement
+ * you entered into with iBOXCHAIN inc.
+ */
+package org.williams.project;
+
+import junit.framework.TestCase;
+
+/**
+ * 类描述
+ *
+ * @author Williams
+ * @since 2025-05-09
+ */
+public class Junit3Test extends TestCase {
+
+    public void testHa() {
+        System.out.println("sdaf");
+    }
+
+
+}
